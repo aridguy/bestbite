fd2c7a80c635193d584fb9f3ea72d53a3a645c87
Ehigiepaul01 <paulehigie95@gmail.com>
2022-04-25 17:30:33 +0100

    changes made to the contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
         <Navbar />
         <div className="px-5 mt-5 pl-5 md:px-40">
           <h1 className="text-2xl text-red-600 font-bold mb-3">Bestbite</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <p>Email us with any questions or inquires or call 518-873-9371. We would be happy to answer your questions and set up a meeting with you. Black Sheep Web design can help you apart from the flock!
           </p>
             <br />
             <h1 className="font-semibold text-lg mb-5">LOCATE US TODAY</h1>
