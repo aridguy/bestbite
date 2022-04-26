48d0f396041004e3026b6fb37e4ae28f4faec00a
aridguy <rotimiariyo@gmail.com>
2022-04-26 02:56:27 +0100

    i modified the footer, the text on the contact page i also created a css file for the footer and i created a separate footer component for the product footer

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import Footer from '../Components/Footer';
 import Navbar from "../Components/Navbar";
-import Map from "../Map/map.jpeg";
+
+
 
 function Contact() {
   return (
     <div>
-        <Navbar />
-        <div className="px-5 mt-5 pl-5 md:px-40">
-          <h1 className="text-2xl text-red-600 font-bold mb-3">Bestbite</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-            <br />
-            <h1 className="font-semibold text-lg mb-5">LOCATE US TODAY</h1>
-            <div className="mb-5 md:justify-center flex items-center">
-              <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1110.9328906696633!2d3.9625187647478537!3d7.457270791391303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039ecf3e6eea7bd%3A0xf1d6ae272af91f44!2sSide%20Hustle!5e1!3m2!1sen!2sng!4v1650897960972!5m2!1sen!2sng' width="100%" height="450"></iframe>
-            </div>
-            <div class="block p-6 rounded-lg shadow-lg bg-white mb-10 mt-15">
-        <form className="">
-          <div class="grid grid-cols-2 gap-4">
-            <div class="form-group mb-6">
-              <input type="text" class="form-control
+      <Navbar />
+      <div className="px-5 mt-5 pl-5 md:px-40">
+        <h1 className="text-2xl text-red-600 font-bold mb-3" align="center">Bestbite</h1>
+        <p>Bestbite is is a Nigerian restaurant chain headquartered in Abuja Nigeria Capital City, that specializes in african dishes and continental dishes. It is the world's second-largest restaurant chain after McDonald's,
+          with 45,621 locations globally in 150 countries as of December 2021. The chain is a subsidiary of SweetBite Restaurant. Bestbite has over 50 thausand workers globally.</p>
+        <br />
+        <div className=' m-10 grid grid-cols-2 gap-3'>
+          <div className='text-red-700' align="justify">Our mission is to leverage our experience and expertise to build the best restaurant on
+            the planet and create the best taste like never before, also build relationships with
+            every single customers.</div>
+          <div className=''> 
+          </div>
+          <div className=''>
+          the planet and create the best taste like never before, also build relationships with
+            every single customers.
+          </div>
+        </div>
+
+        <h1 className="font-semibold text-lg mb-13 mt-30">LOCATE US TODAY</h1>
+        <div className="mb-5 md:justify-center flex items-center">
+          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1110.9328906696633!2d3.9625187647478537!3d7.457270791391303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039ecf3e6eea7bd%3A0xf1d6ae272af91f44!2sSide%20Hustle!5e1!3m2!1sen!2sng!4v1650897960972!5m2!1sen!2sng' width="100%" height="450"></iframe>
+        </div>
+        <div class="block p-6 rounded-lg shadow-lg bg-white mb-10 mt-15 my-9">
+          <form className="">
+            <div class="grid grid-cols-2 gap-4">
+              <div class="form-group mb-6">
+                <input type="text" class="form-control
                 block
                 w-full
                 px-3
@@ -34,10 +48,10 @@ function Contact() {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput123"
-                aria-describedby="emailHelp123" placeholder="First name" />
-            </div>
-            <div class="form-group mb-6">
-              <input type="text" class="form-control
+                  aria-describedby="emailHelp123" placeholder="First name" />
+              </div>
+              <div class="form-group mb-6">
+                <input type="text" class="form-control
                 block
                 w-full
                 px-3
@@ -52,11 +66,11 @@ function Contact() {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput124"
-                aria-describedby="emailHelp124" placeholder="Last name" />
+                  aria-describedby="emailHelp124" placeholder="Last name" />
+              </div>
             </div>
-          </div>
-          <div class="form-group mb-6">
-            <input type="email" class="form-control block
+            <div class="form-group mb-6">
+              <input type="email" class="form-control block
               w-full
               px-3
               py-1.5
@@ -70,15 +84,16 @@ function Contact() {
               ease-in-out
               m-0
               focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput125"
-              placeholder="Email address" />
-          </div>
-          <div class="form-group form-check text-center mb-6">
-            <input type="checkbox"
-              class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-              id="exampleCheck25"  />
-            <label class="form-check-label inline-block text-gray-800" for="exampleCheck25">Subscribe to our newsletter</label>
-          </div>
-          <button type="submit" class="
+                placeholder="Email address" />
+            </div>
+            <div class="form-group form-check text-center mb-6">
+              <input type="checkbox"
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                id="exampleCheck25" />
+              <label class="form-check-label inline-block text-gray-800" for="exampleCheck25">Subscribe to our newsletter</label>
+            </div>
+            <button type="submit" class="
+            my-8
             w-full
             px-6
             py-2.5
@@ -96,9 +111,10 @@ function Contact() {
             transition
             duration-150
             ease-in-out">Sign up</button>
-        </form>
-</div>
+          </form>
         </div>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -7,41 +7,47 @@ import Picture from "../Picture/Rice.jpeg";
 import Drink from "../Drink/drinks.jpeg";
 import mega from "../mega/Eba & soup.jpeg";
 import Footer from "../Components/Footer";
+import FooterProduct from '../Components/FooterProduct';
 
 function Products() {
   return (
     <div>
-        <Navbar />
-        <div className="">
-          <div className="mt-5 pl-5 font-semibold text-red-600 mb-3 md:text-xl md:ml-36"><h1>Deal of the day</h1></div>
-        </div>
-        <Card 
-          title="Pizza"
-          price="$20.50"
-          src={image}
-        />
-        <div className="mt-5 pl-5 font-semibold text-red-600 mb-3 md:text-xl md:ml-36"><h1>Other available products</h1></div>
-        <Card 
-          title="Rice"
-          price="$2.50"
-          src={Picture}
-        />
-        <Card
-          title="Ice cream"
-          price="$3.50"
-          src={images}
-        />
-        <Card 
-          title="Drinks"
-          price="$2.00"
-          src={Drink}
-        />
-        <Card 
-          title="Eba and Soup"
-          price="$10.00"
-          src={mega}
-        />
-        <Footer />
+      
+      <Navbar />
+      <div>
+      <div className="mb-4">
+        <div className="mt-5 pl-5 font-semibold text-red-600 mb-3 md:text-xl md:ml-36"><h1>Deal of the day</h1></div>
+      </div>
+      <Card
+        title="Pizza"
+        price="$20.50"
+        src={image}
+      />
+      <div className="mt-5 pl-5 font-semibold text-red-600 mb-3 md:text-xl md:ml-36"><h1>Other available products</h1></div>
+      <Card
+        title="Rice"
+        price="$2.50"
+        src={Picture}
+      />
+      <Card
+        title="Ice cream"
+        price="$3.50"
+        src={images}
+      />
+      <Card
+        title="Drinks"
+        price="$2.00"
+        src={Drink}
+      />
+      <Card 
+        title="Eba and Soup"
+        price="$10.00"
+        src={mega}
+      />
+      </div>
+      <div>
+      <FooterProduct />
+      </div>
     </div>
   )
 }
