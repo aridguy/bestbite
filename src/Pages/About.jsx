41a9a547639647710b897d9306fd3acf8f1f69ed
RellzCodez <adeyemiodunayo21@gmail.com>
2022-04-26 08:55:46 -0700

    "added to the about component"

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,8 +6,12 @@ function About() {
     <div>
         <Navbar />
         <div className="px-5 mt-5 mb-10 pl-5 text-lg md:px-40">
-          <h1><span className="text-red-600 font-bold">Bestbite</span> is a blah blah blah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <h1><span className="text-red-600 font-bold">Bestbite</span> Best Bite believes that great food are made from the heart and so we provide our customers with so many options for everyone to enjoy including homemade cooked meals like Swallow and all kinds of Soups, whilst still providing fast food like pizza, burgers etc. We also offer a wide variety of side order. Everything is available for dining take out or home delivery. 
+
+Best Bite appeals to a broad range of customers who enjoy the best of fast food at an amazing speed and affordable price.
+
+Founded in April of 2022 and based remotely in Nigeria. Best Bite is a trusted food service platform for people wanting a trusted and reliable feeding structure. We uphold a culture of trust and excellence through our product, and service experience. By creating a world where anyone can order food with their phones and then be assured to have it received safely.
+
           </h1>
         </div>
     </div>
