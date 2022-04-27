521ad41a7fbcebaf693c0347435c609fdda296e4
George Olufemi <georgeoluolufemi@gmail.com>
2022-04-27 11:46:16 +0100

    Edited the About Page and the Contact page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import Navbar from "../Components/Navbar";
-import Footer from '../Components/Footer';
+import Footer from "../Components/Footer";
 
 function About() {
   return (
     <div>
         <Navbar />
-        <div className="px-5 mt-5 mb-10 pl-5 text-lg md:px-40">
-          <h1><span className="text-red-600 font-bold">Bestbite</span> Best Bite believes that great food are made from the heart and so we provide our customers with so many options for everyone to enjoy including homemade cooked meals like Swallow and all kinds of Soups, whilst still providing fast food like pizza, burgers etc. We also offer a wide variety of side order. Everything is available for dining take out or home delivery. 
-
-Best Bite appeals to a broad range of customers who enjoy the best of fast food at an amazing speed and affordable price.
-
-Founded in April of 2022 and based remotely in Nigeria. Best Bite is a trusted food service platform for people wanting a trusted and reliable feeding structure. We uphold a culture of trust and excellence through our product, and service experience. By creating a world where anyone can order food with their phones and then be assured to have it received safely.
-
+        <div className="px-5 mt-5 mb-20 pl-5 text-lg md:px-40 tracking-wider leading-loose">
+          <h1><span className="text-red-600 font-bold">Bestbite</span> believes that great foods are made from the heart and so we provide our customers with so many options for everyone to enjoy including homemade cooked meals like Swallow and all kinds of Soups, whilst still providing fast food like pizza, burgers etc. We also offer a wide variety of side orders. Everything is available for dining take out or home delivery.<br /> 
+          Best Bite appeals to a broad range of customers who enjoy the best of fast food at an amazing speed and affordable price.
+          Founded in April of 2022 and based remotely in Nigeria.<br /> Best Bite is a trusted food service platform for people wanting a trusted and reliable feeding structure.<br /> 
+          We uphold a culture of trust and excellence through our product, and service experience. By creating a world where anyone can order food with their phones and then be assured to have it received safely.
           </h1>
         </div>
         <Footer />
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import Footer from '../Components/Footer';
 import Navbar from "../Components/Navbar";
+import "./Contacts.css"
 
 function Contact() {
   return (
     <div>
       <Navbar />
-      <div className="px-5 mt-5 pl-5 md:px-40">
-        <h1 className="text-2xl text-red-600 font-bold mb-3">Bestbite</h1>
-        <p className="mb-7">Bestbite is a Nigerian restaurant chain headquartered in Abuja Nigeria Capital City, that specializes in african dishes and continental dishes. It is the world's second-largest restaurant chain after McDonald's,
-          with 45,621 locations globally in 150 countries as of December 2021. The chain is a subsidiary of SweetBite Restaurant. Bestbite has over 50 thousand workers globally. Our mission is to leverage our experience and expertise to build the best restaurant on the planet and create the best taste like never before, also build relationships with every single customers.</p>
-        <h1 className="font-semibold text-lg mb-13 mt-30">LOCATE US TODAY</h1>
-        <div className="mb-5 md:justify-center flex items-center">
-          <iframe className="mt-7" src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1110.9328906696633!2d3.9625187647478537!3d7.457270791391303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039ecf3e6eea7bd%3A0xf1d6ae272af91f44!2sSide%20Hustle!5e1!3m2!1sen!2sng!4v1650897960972!5m2!1sen!2sng' width="100%" height="450"></iframe>
+      <div className='container-fluid px-5'>
+        <div className='row'>
+          <div className='col-md-12 col-sm-12'>
+            <h1 className='contactTitle'align="center">Bestbite</h1>
+            <div align="center"><span className='location-text' align="center">See our actual location in Nigeria, our representatives all online 24/7 and ready to take your orders and enquries</span></div>
+            <p align="center"> <hr className='small-line' /></p>
+          </div>
         </div>
-        <div class="block p-6 rounded-lg shadow-lg bg-white mb-10 mt-15 my-9">
-          <form className="">
-            <div class="grid grid-cols-2 gap-4">
-              <div class="form-group mb-6">
-                <input type="text" class="form-control
+        <div className='row row2'>
+          <div className='col-md-4'>
+            <div>
+              <p className='content1'>
+                Bestbite is a Nigerian restaurant chain headquartered in Abuja, Nigeria's Capital City, that specializes in African dishes and inter-continental dishes.
+              </p>
+              <p className='content2'>
+                It is the world's second-largest restaurant chain after McDonald's, with 45,621 locations globally in 150 countries as of December 2021.
+              </p> <br />
+              <p className='content3'>
+                info@bestbite.com.ng
+              </p> <br />
+              <p className='content3'>
+                The chain is a subsidiary of SweetBite
+                Restaurant. Bestbite has over 50 thausand workers globally.</p> <br />
+              <p className='content3'> 
+                <b>Get in touch</b> <br />
+                Curious about something? We're here to help.
+                We do our best to respond to emails and our Live chat feature on the webpage, within 48 hours (and are even faster on Instagram).
+              </p> <br />
+              <p className='content4'>
+                <button className='my-8
+            w-full
+            px-6
+            py-2.5
+            bg-red-600
+            text-white
+            font-medium
+            text-xs
+            leading-tight
+            uppercase
+            rounded
+            shadow-md
+            hover:bg-red-500 hover:shadow-lg
+            focus:bg-red-500 focus:shadow-lg focus:outline-none focus:ring-0
+            active:bg-red-500 active:shadow-lg
+            transition
+            duration-150
+            ease-in-out'>Explore</button>
+              </p>
+            </div>
+          </div>
+          <div className='col-md-4'>
+
+            <div>
+              <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1110.9328906696633!2d3.9625187647478537!3d7.457270791391303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1039ecf3e6eea7bd%3A0xf1d6ae272af91f44!2sSide%20Hustle!5e1!3m2!1sen!2sng!4v1650897960972!5m2!1sen!2sng' width="100%" height="350"></iframe>
+              <br />
+              <p>E-mail: bestbite@gmail.com <br />
+                Phone number: +234 80 000 0000</p>
+              <span>
+                You can also visit our website at <b><i>https://bestbitee.netlify.app/</i></b> our website is open to everyone and all orders can be placed from the website.
+              </span>
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <h1 className='contact-title2'>CONTACT US TODAY</h1>
+            <div class="block p-6 rounded-lg shadow-lg bg-white mb-10 mt-15 my-9">
+              <form className="">
+                <div class="grid grid-cols-2 gap-4">
+                  <div class="form-group mb-6">
+                    <input type="text" class="form-control
                 block
                 w-full
                 px-3
@@ -33,10 +90,10 @@ function Contact() {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput123"
-                  aria-describedby="emailHelp123" placeholder="First name" />
-              </div>
-              <div class="form-group mb-6">
-                <input type="text" class="form-control
+                      aria-describedby="emailHelp123" placeholder="First name" />
+                  </div>
+                  <div class="form-group mb-6">
+                    <input type="text" class="form-control
                 block
                 w-full
                 px-3
@@ -51,11 +108,11 @@ function Contact() {
                 ease-in-out
                 m-0
                 focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput124"
-                  aria-describedby="emailHelp124" placeholder="Last name" />
-              </div>
-            </div>
-            <div class="form-group mb-6">
-              <input type="email" class="form-control block
+                      aria-describedby="emailHelp124" placeholder="Last name" />
+                  </div>
+                </div>
+                <div class="form-group mb-6">
+                  <input type="email" class="form-control block
               w-full
               px-3
               py-1.5
@@ -69,15 +126,12 @@ function Contact() {
               ease-in-out
               m-0
               focus:text-gray-700 focus:bg-white focus:border-red-600 focus:outline-none" id="exampleInput125"
-                placeholder="Email address" />
-            </div>
-            <div class="form-group form-check text-center mb-6">
-              <input type="checkbox"
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-red-600 checked:border-red-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-                id="exampleCheck25" />
-              <label class="form-check-label inline-block text-gray-800" for="exampleCheck25">Subscribe to our newsletter</label>
-            </div>
-            <button type="submit" class="
+                    placeholder="Email address" />
+                </div>
+                <div class="form-group subscribeNletter form-check text-left mb-6">
+                  <input type="checkbox" /> <span>subscribe to our news letter</span>
+                </div>
+                <button type="submit" class="
             my-8
             w-full
             px-6
@@ -96,7 +150,9 @@ function Contact() {
             transition
             duration-150
             ease-in-out">Sign up</button>
-          </form>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
